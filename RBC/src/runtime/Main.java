f57a418b88c8b0bf275a60f6669e8e37943434d8
danilo.diablo@gmail.com <danilo.diablo@gmail.com@0efa33cd-d942-6766-a8b5-532f7129b88a>
2013-03-26 00:58:54 +0000

    Banco inicial com 31 casos. Durante a execução é possível cadastrar novos casos digitando: "Cadastrar"

--- RBC/src/runtime/Main.java
+++ RBC/src/runtime/Main.java
@@ -25,23 +25,25 @@ public class Main {
         if (opc.equals("1")) {
             try {
                 BufferedReader in = new BufferedReader(new FileReader(args[0]));
+                System.out.print("Cadastrando casos");
                 while (in.ready()) {
                     //Leia a linha
                     String str = in.readLine();
                     if (str.startsWith("Descricao: ")) {
                         Caso caso = new Caso(new Entrada(str.substring(str.indexOf("Descricao:") + "Descricao: ".length(), str.indexOf("Solucao:"))));
-                        System.out.println("DESCRICAO:" + caso.getProblema().getTexto());
+                        //System.out.println("DESCRICAO:" + caso.getProblema().getTexto());
                         if (str.contains("Solucao: ")) {
                             caso.setSolucao(new Saida(str.substring(str.indexOf("Solucao:") + "Solucao: ".length(), str.indexOf("Avaliacao:"))));
-                            System.out.println("SOLUCAO:" + caso.getSolucao().getTexto());
+                            //System.out.println("SOLUCAO:" + caso.getSolucao().getTexto());
 
                             if (str.contains("Avaliacao: ")) {
                                 caso.setAvaliacao(Integer.parseInt(str.substring(str.indexOf("Avaliacao:") + "Avaliacao: ".length())));
                                 if (caso.getAvaliacao() == 1) {
-                                    System.out.println("AVALICAO:Boa");
+                                    //System.out.println("AVALICAO:Boa");
                                 }
-                                System.out.println(caso);
+                                //System.out.println(caso);
                                 banco.cadastrarCaso(caso);
+                                System.out.print(".");
                             }
                         }
 
@@ -51,22 +53,53 @@ public class Main {
                 System.out.println("Coloque o novo caso no mesmo diretório do programa e então re-execute, passando o arquivo como parâmetro");
                 System.out.println(e);
             }
+            System.out.println("\nTodos os casos cadastrados com sucesso!");
 
         } else if (opc.equals("2")) {
             Boolean condicao = true;
 
-            System.out.println("Bem vindo ao RBC - Xaat, a partir de agora você pode conversar com o sistema. \nCaso deseje sair digite Sair.");
+            System.out.println("Bem vindo ao RBC - Xaat, a partir de agora você pode conversar com o sistema. \nPara cadastrar um novo caso, digite Cadastrar. Caso deseje sair digite Sair.");
             while (condicao == true) {
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                 try {
                     String entrada = in.readLine();
-                    System.out.println("Você disse: " + entrada);
                     if (entrada.equalsIgnoreCase("Sair")) {
                         condicao = false;
-                    } else {
+                    } 
+                    else if(entrada.equalsIgnoreCase("Cadastrar")){
+                    	System.out.println("Descreva o caso");
+                    	BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+                    	String str = buff.readLine();
+						Caso caso = new Caso(new Entrada(str));
+						System.out.println("Proponha uma solucao/resposta");
+						String str_solucao = buff.readLine();
+						
+						caso.setSolucao(new Saida(str_solucao));
+						System.out.println("Como voce avalia sua solucao? Boa ou Ruim");
+						String confirm_avaliacao = buff.readLine();
+						if (confirm_avaliacao.equalsIgnoreCase("Boa") || confirm_avaliacao.equalsIgnoreCase("B") ){
+							caso.setAvaliacao(1);
+							}
+						else {
+							caso.setAvaliacao(0);
+							}
+						System.out.println("Descricao: "+caso.getProblema());
+						System.out.println("Solucao: "+ caso.getSolucao());
+						System.out.println("Avaliacao: "+ confirm_avaliacao);
+						System.out.println("Tudo certo?");
+						String scan_confirm = buff.readLine();
+						
+						if (scan_confirm.equalsIgnoreCase("Sim") || scan_confirm.equalsIgnoreCase("S")) {
+							banco.cadastrarCaso(caso);
+							System.out.println("Caso adicionado ao banco!");}
+						
+						
+	                    }
+                    
+                    else {
                         Caso caso = new Caso(new Entrada(entrada));
                         Caso caso_recuperado = banco.casoSemelhante(caso);
-                        System.out.println("Match: " + banco.casoSemelhanteMatch(caso));
+                        //System.out.println("Match: " + banco.casoSemelhanteMatch(caso));
                         if (caso_recuperado != null) {
                             if (caso_recuperado.getAvaliacao() == 1) {
                                 caso.setSolucao(caso_recuperado.getSolucao());
@@ -79,22 +112,27 @@ public class Main {
                             System.out.println("Por favor, avalie minha resposta. Responda Sim ou Nao");
                             String esc = scan.next();
                             if (esc.equalsIgnoreCase("Sim") || esc.equalsIgnoreCase("S")) {
-                                System.out.println("A solucao oferecia por mim, foi boa? Se sim, responda Sim ou s");
+                                System.out.println("A solucao oferecia por mim, foi boa? Se sim, responda Sim ou s. Ou Nao ou n.");
                                 String a = scan.next();
                                 if (a.equalsIgnoreCase("Sim") || a.equalsIgnoreCase("S")) {
                                     caso.setAvaliacao(1);
+                                    if (banco.casoSemelhanteMatch(caso) <= 0.6) {
+                                        banco.cadastrarCaso(caso);
+                                        System.out.println("Novo caso adicionado ao banco!");
+                                    }
 
-                                } else {
+                                } else if(a.equalsIgnoreCase("Nao") || a.equalsIgnoreCase("N")) {
                                     caso.setAvaliacao(0);
+                                    if (banco.casoSemelhanteMatch(caso) <= 0.6) {
+                                        banco.cadastrarCaso(caso);
+                                        System.out.println("Novo caso adicionado ao banco!");
+                                    }
                                 }
-                                if (banco.casoSemelhanteMatch(caso) <= 0.6) {
-                                    banco.cadastrarCaso(caso);
-                                    System.out.println("Novo caso adicionado ao banco!");
-                                }
+                                
                             }
 
                         } else {
-                            System.out.println("Xaat diz: \"Opa, não sei o que falar!\"");
+                            System.out.println("Xaat diz: \"Ahhh lelek lek lek lek lek...Oi? Eu estava cantando...\"");
                         }
 
                     }
